2cd852915f9751c30c1465bda1710af42d6d368e
Aiezer <aiezer.santos01@gmail.com>
2022-10-28 16:59:42 -0300

    Feat: criado um utilitário para encriptar e comparar a senha enviada do front com a presente no DB.

--- app/backend/src/utils/encryptPassword.ts
+++ app/backend/src/utils/encryptPassword.ts
@@ -2,12 +2,14 @@ import * as bcrypt from 'bcryptjs';
 
 require('express-async-errors');
 
+/* This is a class that is used to check the password. */
 export default class encryptPassword {
-  static checkPassword(inputPassword:string, passwordHash:string) {
+  static checkPassword(inputPassword:string, passwordHash:string): void | boolean {
     const check = bcrypt.compareSync(inputPassword, passwordHash);
 
     if (!check) {
       throw new Error('401|Incorrect email or password');
     }
+    return true;
   }
 }
