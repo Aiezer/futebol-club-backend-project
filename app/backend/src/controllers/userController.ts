bbd3ae81702e5b6836919d348699714fe5ca2bbb
Aiezer <aiezer.santos01@gmail.com>
2022-10-28 18:19:21 -0300

    Feat: criado o controller para realização do login

---
+++ app/backend/src/controllers/userController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import UserService from '../database/models/services/UserService';
+import { IUserLogin } from '../database/models/entities/interfaces/IUser';
+
+export default class UserController {
+  constructor(private userService: UserService) {}
+
+  public login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const result = await this.userService.login(req.body as IUserLogin);
+      res.status(200).json({
+        token: result,
+      });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  };
+}
