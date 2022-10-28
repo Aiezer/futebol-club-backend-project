5ac664fb4ac3d43562d8ad961e8bc1afadd456ea
Aiezer <aiezer.santos01@gmail.com>
2022-10-28 17:34:56 -0300

    Fix: Foram realizados alguns ajustes na forma de validar os dados de entrada para o login.

--- app/backend/src/database/models/services/UserService.ts
+++ app/backend/src/database/models/services/UserService.ts
@@ -4,24 +4,25 @@ import UserRepository from '../repository/UserRepository';
 import { encryptPassword, Jwt } from '../../../utils';
 
 export default class UserService {
-  constructor(
-    private userRepository = new UserRepository(),
-  ) {}
+  constructor(private userRepository = new UserRepository()) {}
 
   login = async (login: IUserLogin) => {
-    const { email } = login;
-    const checkEmail = UserValidation.validateUser(email);
-    if (!checkEmail) {
-      throw new Error('401|Incorrect email or password');
+    const message = [
+      '401|Incorrect email or password',
+      '400|All fields must be filled',
+    ];
+
+    const checkFields: number = UserValidation.validateUser(login);
+    if (checkFields !== -1) {
+      throw new Error(message[checkFields]);
     }
 
     const user = await this.userRepository.handleLogin(login);
     if (!user) {
-      throw new Error('401|Incorrect email or password');
+      throw new Error(message[0]);
     }
 
     const { id, username, password } = user;
-
     encryptPassword.checkPassword(login.password, password);
 
     const token = Jwt.createToken({ id, username });
--- app/backend/src/validations/UserValidation.ts
+++ app/backend/src/validations/UserValidation.ts
@@ -1,12 +1,27 @@
+import { IUserLogin } from '../database/models/entities/interfaces/IUser';
+
 export default class UserValidation {
+  public static validateFields(login: IUserLogin): boolean {
+    const { email, password } = login;
+    if (email.length > 0 && password.length > 0) {
+      return true;
+    }
+    return false;
+  }
+
   private static validateEmail(email: string): boolean {
     const emailRegex = /\S+@\S+\.\S+/;
     return emailRegex.test(email);
   }
 
-  public static validateUser(email: string): boolean {
-    return (
-      this.validateEmail(email)
-    );
+  public static validateUser(login: IUserLogin): number {
+    if (!this.validateFields(login)) {
+      return 1;
+    }
+    const { email } = login;
+    if (!this.validateEmail(email)) {
+      return 0;
+    }
+    return -1;
   }
 }
