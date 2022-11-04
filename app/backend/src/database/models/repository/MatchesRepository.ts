168ba651aaa46d046f812dca9414540f7da66f69
Aiezer <aiezer.santos01@gmail.com>
2022-11-03 21:48:02 -0300

    Feat: criado o Matches Repository, que conterá todas as funções de acesso à tabela de matches.

---
+++ app/backend/src/database/models/repository/MatchesRepository.ts
@@ -0,0 +1,16 @@
+import { IMatch } from '../entities/interfaces/IMatch';
+import Matches from '../entities/Matches';
+import Teams from '../entities/Teams';
+
+export default class MatchesRepository {
+  getAll = async (): Promise<IMatch[]> => {
+    const matches = await Matches.findAll({
+      include: [
+        { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+        { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    return matches as IMatch[];
+  };
+}
---
+++ app/backend/src/database/models/repository/index.ts
@@ -0,0 +1,5 @@
+import UserRepository from './UserRepository';
+import TeamsRepository from './TeamsRepository';
+import MatchesRepository from './MatchesRepository';
+
+export { UserRepository, TeamsRepository, MatchesRepository };
